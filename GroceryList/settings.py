04005d4bba7b7de4d533c73265dd9af60d7ccca5
Ponharish <e1123017@u.nus.edu>
2024-12-17 22:14:47 +0800

    Update settings

--- GroceryList/settings.py
+++ GroceryList/settings.py
@@ -125,7 +125,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-# CSRF_COOKIE_SECURE = False  
-# CSRF_USE_SESSIONS = False
-# CSRF_COOKIE_HTTPONLY = False
